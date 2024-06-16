5a5a3938a8f4d4f8730c13e611bc5f59041b4669
Arpit Agrawal <arpit_agr0123@yahoo.com>
2024-06-16 11:09:22 +0530

    First commit

---
+++ First_code.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls') 
+student1 = {"name":"Arpit", "year":2024}
+print(student1)
+student2 = {"name":"Aman", "year":2023}
+print(student2)
+student_All = {student1, student2}
+print(student_All)
